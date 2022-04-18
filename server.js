12a86836258b54ed7ed00d1b5443315fee0c54b3
Inthat Boonpongmanee <ib@Inthats-MacBook-Pro.local>
2022-04-18 18:53:40 -0400

    updated

--- server.js
+++ server.js
@@ -1,6 +1,45 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import pool from './db.js'
+import cors from 'cors' //+ NEW
+
+const app = express()
+const PORT = process.env.PORT || 4444
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+
+//+ NEW //////////////////////////////////////////////////////////////
+
+app.use(cors())
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+//+ NEW //////////////////////////////////////////////////////////////
 
 
-app.post('results', (req, res) => {
-    // append result to a file
-    FileSystem.appendFile('filename.csv', req.body)
+app.post('/', (req, res) => {
+
+    console.log(req.body)
+
+    // send data to postgres db
+    pool.query('INSERT INTO Results(data) VALUES ($1)', [req.body], (err, res) => {
+        if(err){
+            console.log('error inserting data:')
+            console.log(err)
+        }
+    })
+
+
+    res.status(200).json('success')
+
+})
+
+app.listen(PORT, ()=> {
+    console.log('app listening on', PORT)
+    console.log('=========='+ Date.now() +'=============')   
 })
